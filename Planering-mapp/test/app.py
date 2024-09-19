8dbfd19ab41dab3902335255a0b958570be7e85a
addefrr <arqe24@student.bth.se>
2024-09-19 11:19:37 +0200

    Changed the checkbox to radio
    I changed the checkbox function in pickup/delivery to a radio function, which ensures that the user can only pick one of the options.

--- Planering-mapp/test/app.py
+++ Planering-mapp/test/app.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 # Variable to store the shared data
 shared_data = {}
 
-# Route for Frontend A to send data
+# Route for BurgerOrderer to send data
 @app.route('/send-data', methods=['POST'])
 def send_data():
     global shared_data
     shared_data = request.json  # Store the received JSON data
     return jsonify({'message': 'Data received'})  # Respond with a confirmation
 
-# Route for Frontend B to get the stored data
+# Route for KitchenView to get the stored data
 @app.route('/get-data', methods=['GET'])
 def get_data():
     return jsonify(shared_data)  # Return the stored data as JSON
 
-# Serving the Frontend A and Frontend B HTML pages
+# Serving the BurgerOrderer and KitchenView HTML pages
 @app.route('/burgerorderer')
 def burgerorderer():
     return render_template('burgerorderer.html')
