0e54c412ce6749fc04ce055c6b265a63d911013a
addefrr <arqe24@student.bth.se>
2024-09-20 08:49:24 +0200

    Automation
    Somewhat automated the process of running it.

---
+++ System/startup.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+# Define the URL you want to open
+url_kitchenview = "http://127.0.0.1:5000/kitchenview"
+url_burgerorderer = "http://127.0.0.1:5000/burgerorderer"
+
+# Open the URL in the default web browser
+webbrowser.open(url_kitchenview)
+webbrowser.open(url_burgerorderer)
